1981c43782304f31da73179d0354e544e26913bd
Your Name <you@example.com>
2024-12-26 15:59:08 +0400

    shell

---
+++ shellbety.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    /* Printing "Hello, World!" to the console */
+    printf("Hello, World!\n");    
+    
+    return (0);
+}
